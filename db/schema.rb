caa2746d55a778ae50268e1502e36f0c4ff64bc3
trustpizza <maxelolsson@gmail.com>
2022-12-03 17:13:35 -0600

    Add usernames

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_204400) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_230847) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_204400) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
