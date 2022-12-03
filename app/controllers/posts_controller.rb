421304db919272e20308fe22e7e1862823afdc75
trustpizza <maxelolsson@gmail.com>
2022-12-03 15:01:58 -0600

    Create new post form, begin work on create method in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,22 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def new
-    @user = User.new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_member.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        puts 'hi'
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
